542ac6f1988a772f9a535a497b705d6b5c0bd064
chaosagent <davidh@cpu2.co.cc>
2012-09-21 16:57:25 -0700

    added roms page

---
+++ roms/liberty-sr/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Roms</title>
+<script type="text/javascript" src="http://randomstuffs.co.cc/analytics.js"></script>
+</head>
+<body>
+9/21/12
+<br>
+<a href="https://docs.google.com/open?id=0B2qlFDXo6JOrMGFQTlVvSDlFM2s">Download</a>
+<?php include("http://randomstuffs.co.cc/ads.html"); ?>
+</body>
+</html>
